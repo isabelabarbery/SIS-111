b842c6a144cf4a62775904e8403e56e124d59792
isabelabarbery <isabela.barbery@ucb.edu.bo>
2025-04-10 09:48:18 -0400

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+
+#CREAR UNA VAR
+saludo = "Hola Mundo  !!!"
+print (saludo) 
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print(suma)
+      
+#suma de palabras -> concatenar palabras 
+palabra1="hola"
+palabra2="mundo"
+suma_palabras = palabra1+" " +palabra2
+print (suma_palabras)
+#resta
+var1=10
+var2=4
+resta=var1-var2
+print(resta)
+#multiplicacion
+base=6
+altura=10
+area=base*altura
+print(area)
+#division
+var1=15
+var2=2
+dividir=var1/var2
+print(dividir)
